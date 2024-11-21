662de28440748222b5ef2366575349d6e5e6d034
RadiTEFA <radingoding@gmail.com>
2024-11-21 14:34:49 +0700

    Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,42 +1,33 @@
 <template>
       <nav class="navbar header fixed-top" id="myHeader" style="background-color: #E5B844; height: 10vh;">
             <div class="container-fluid dua">
-               <div class="d-flex mb-3"></div>
                <div type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-                  <div class="offcanvas-header">
-                     <div data-bs-dismiss="offcanvas">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                        </svg>
-                     </div>
-                  </div>
+                  <i style="font-size: 40px" class="bi bi-list"></i>
                </div>
                <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-                  <div class="offcanvas-header satu">
-                     <div data-bs-dismiss="offcanvas">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list Menu" viewBox="0 0 16 16">
-                           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                        </svg>
-                     </div>
-                     <div class="Text-Menu ps-5" >
-                        <p><b>Menu</b></p>
-                     </div>
-                     <hr>
+               <div class="offcanvas-header">
+                  <div type="butoon" style="height: 70px" data-bs-dismiss="offcanvas" aria-label="Menu">
+                     <i style="font-size: 40px" class="bi bi-list ps-3"></i>
                   </div>
-                  <hr>
-                  <div class="offcanvas-body">
+                  <div class="Text-Menu offcanvas-title" id="staticBackdropLabel">
+                     <p><b>Menu</b></p>
+                  </div>
+               </div>
+               <div class="offcanvas-body">
+                  <div class="isimenu">
                      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item ps-3">
                            <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item ps-3">
-                           <a class="nav-link active" aria-current="page" href="#">Home</a>
+                           <a class="nav-link active" aria-current="page" href="#">Riwayat</a>
                         </li>
                         <li class="nav-item ps-3">
-                           <a class="nav-link active" aria-current="page" href="#">Home</a>
+                           <a class="nav-link active" aria-current="page" href="#">Profile</a>
                         </li>
                      </ul>
                   </div>
+               </div>
                </div>
                <div class="p-2"><strong class="tiga">ABSENSI KELAS</strong></div>
                <div class="ms-auto p-2"><img :src="fotoProfil" width="50px" height="50px"></div>
@@ -73,10 +64,7 @@ onMounted(() => {
    text-align: center;
    justify-content: center;
    align-items: center;
-   margin-top: 20%;
-   margin-bottom: 11%;
-   margin-right: 25%;
-   font-size: 30%;
+   padding-right: 40%;
 }
 .dua{
    font-size: 30px;
@@ -90,5 +78,8 @@ onMounted(() => {
 }
 img{
    border-radius: 25px;
+}
+.isimenu li a{
+   font-size: 20px;
 }
 </style>
